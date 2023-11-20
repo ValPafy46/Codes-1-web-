8cf4998e4d6fcc0edf350e230f1792d95c7caac8
ValPafy46 <valentino46lopes@gmail.com>
2023-11-20 17:49:54 +0000

    Update

--- script.js
+++ script.js
@@ -42,3 +42,34 @@ function hideElement(element) {
 
 // Oculta a mensagem de sucesso ao carregar a página
 hideElement(document.getElementById('successMessage'));
+
+function updateCountdown() {
+    const countdownDate = new Date("2023-11-21T23:59:00").getTime();
+
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    const countdownElement = document.getElementById("countdown");
+    
+    if (distance > 0) {
+        countdownElement.innerHTML = `
+            <span class="days">${days}d</span>
+            <span class="hours">${hours}h</span>
+            <span class="minutes">${minutes}m</span>
+            <span class="seconds">${seconds}s</span>
+        `;
+    } else {
+        clearInterval(interval);
+        countdownElement.classList.add("expired");
+        countdownElement.textContent = "EXPIRED";
+    }
+}
+
+const interval = setInterval(updateCountdown, 1000);
+updateCountdown();
+
